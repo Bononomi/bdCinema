337430adce85cb55d0c39321a5569b3df1049e79
Bononomi <47350182+Bononomi@users.noreply.github.com>
2019-07-10 19:20:07 -0300

    Update

--- criarTabelaCartaoCredito.sql
+++ criarTabelaCartaoCredito.sql
@@ -2,6 +2,6 @@ CREATE TABLE IF NOT EXISTS cartaoCredito(
  idCartao_Credito INT NOT NULL AUTO_INCREMENT,
  Nome_Cartao VARCHAR(45),
  dataValidade_Cartao DATE,
- NumCartao_Cartao float(16),
+ NumCartao_Cartao string(16),
  Cliente_idCliente INT NOT NULL,
  PRIMARY KEY (idCartao_Credito));
